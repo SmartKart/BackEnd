ed619a691a1d23e6ded89c9ff2be24dd7a5f0bde
Jashan Sudan <jashansudan@gmail.com>
2017-03-18 17:01:20 -0700

    APIs working locallygit add .git add .

---
+++ models/database.js
@@ -0,0 +1,54 @@
+var async = require('async');
+
+// Require the driver.
+var pg = require('pg');
+
+// Connect to the "shoppingCart" database.
+var config = {
+  user: 'maxroach',
+  host: 'localhost',
+  database: 'shoppingCart',
+  port: 26257
+};
+
+
+pg.connect(config, function (err, client, done) {
+  // Closes communication with the database and exits.
+  var finish = function () {
+    done();
+    process.exit();
+  };
+
+  if (err) {
+    console.error('could not connect to cockroachdb', err);
+    finish();
+  }
+  async.waterfall([
+    function (next) {
+      // Create the "cart" table.
+      client.query("CREATE TABLE IF NOT EXISTS cart (id INT PRIMARY KEY, lat INT, long INT);");
+      client.query("CREATE TABLE IF NOT EXISTS store (id INT PRIMARY KEY, name TEXT, price DECIMAL(8,2), percentOff INT, isOnSale BOOLEAN, type TEXT);", next);
+    },
+    function (next) {
+      // Insert two rows into the "cart" table.
+      client.query("INSERT INTO cart (id, lat, long) VALUES (1, 0, 0);");
+    },
+    function (results, next) {
+      // Print out the balances.
+      client.query('SELECT id, lat, long FROM cart;', next);
+    },
+  ],
+  function (err, results) {
+    if (err) {
+      console.error('error inserting into and selecting from cart', err);
+      finish();
+    }
+
+    console.log('Initial balances:');
+    results.rows.forEach(function (row) {
+      console.log(row);
+    });
+
+    finish();
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,241 @@
 var express = require('express');
 var router = express.Router();
+const pg = require('pg');
+const path = require('path');
+const connectionString = process.env.DATABASE_URL || 'postgresql://root@localhost:26257/shoppingCart'
 
+// THIS IS FOR CART DB
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/api/v1/carts', (req, res, next) => {
+  const results = [];
+  // Grab data from http request
+  const data = {id: req.body.id, lat: 0, long: 0};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    client.query('INSERT INTO cart(id, lat, long) values($1, $2, $3)',
+    [data.id, data.lat, data.long]);
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM cart ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+
+router.get('/api/v1/carts', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM cart ORDER BY id ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.put('/api/v1/carts/:cart_id', (req, res, next) => {
+  const results = [];
+  // Grab data from the URL parameters
+  const id = req.params.cart_id;
+  // Grab data from http request
+  const data = {lat: req.body.lat, long: req.body.long};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Update Data
+    client.query('UPDATE cart SET lat=($1), long=($2) WHERE id=($3)',
+    [data.lat, data.long, id]);
+    // SQL Query > Select Data
+    const query = client.query("SELECT * FROM cart ORDER BY id ASC");
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.delete('/api/v1/carts/:cart_id', (req, res, next) => {
+  const results = [];
+  // Grab data from the URL parameters
+  const id = req.params.cart_id;
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Delete Data
+    client.query('DELETE FROM cart WHERE id=($1)', [id]);
+    // SQL Query > Select Data
+    var query = client.query('SELECT * FROM cart ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+
+
+// THIS IS FOR STORE DB
+router.post('/api/v1/store', (req, res, next) => {
+  const results = [];
+  // Grab data from http request
+  const data = {id: req.body.id, name: req.body.name, price: req.body.price, percentOff: req.body.percentOff, isOnSale: req.body.isOnSale, type:req.body.type };
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    client.query('INSERT INTO store(id, name, price, percentOff, isOnSale, type) values($1, $2, $3, $4, $5, $6)',
+    [data.id, data.name, data.price, data.percentOff, data.isOnSale, data.type]);
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM store ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.get('/api/v1/store', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM store ORDER BY id ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.put('/api/v1/store/:item_id', (req, res, next) => {
+  const results = [];
+  // Grab data from the URL parameters
+  const id = req.params.item_id;
+  // Grab data from http request
+  const data = {name: req.body.name, price: req.body.price, percentOff: req.body.percentOff, isOnSale: req.body.isOnSale, type:req.body.type};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Update Data
+    client.query('UPDATE store SET name=($1), price=($2), percentOff=($3), isOnSale=($4), type=($5) WHERE id=($6)',
+    [data.name, data.price, data.percentOff, data.isOnSale, data.type, id]);
+    // SQL Query > Select Data
+    const query = client.query("SELECT * FROM store ORDER BY id ASC");
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.delete('/api/v1/store/:item_id', (req, res, next) => {
+  const results = [];
+  // Grab data from the URL parameters
+  const id = req.params.item_id;
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Delete Data
+    client.query('DELETE FROM store WHERE id=($1)', [id]);
+    // SQL Query > Select Data
+    var query = client.query('SELECT * FROM store ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
 });
 
 module.exports = router;
